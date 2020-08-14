15cd98690c2a40bce248ecc0157bd092d293e794
James Zhang <Jameszhang9795478@gmail.com>
2020-08-14 02:39:02 -0400

    just some updates:

--- driving.py
+++ driving.py
@@ -7,10 +7,12 @@ if country == 'canada':
 		print('you may get a drivers licenis')
 	else: 
 		print('sorry man not old enough')
-if country == 'china':
+elif country == 'china':
 	print('sorry for you buudy no driving for you')
-if country == 'tai wan': 
+elif country == 'tai wan': 
 	if age >= 18:
 		print('you can get a drivers licenis')
 	else:
 		print('too young BOI')
+else:
+	print('fk u ')
